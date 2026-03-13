bd40d61dfea046f4dc1995175433fb082d417dd3
Zerl <1804253012@qq.com>
2026-03-13 17:04:32 +0800

    实现天气查询功能

--- app/src/main/java/com/example/sunnyweather/ui/weather/WeatherActivity.kt
+++ app/src/main/java/com/example/sunnyweather/ui/weather/WeatherActivity.kt
@@ -1,5 +1,6 @@
 package com.example.sunnyweather.ui.weather
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -22,6 +23,12 @@ class WeatherActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val decorView = window.decorView
+        decorView.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        window.statusBarColor = Color.TRANSPARENT
+        setContentView(R.layout.activity_weather)
+
         binding = ActivityWeatherBinding.inflate(layoutInflater)
         setContentView(binding.root)
         if (viewModel.locationLng.isEmpty()) {
